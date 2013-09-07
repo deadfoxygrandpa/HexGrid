b44211e9177968297a067437766cee769fe5955e
Alex Neslusan <deadfoxygrandpa@gmail.com>
2013-09-07 16:27:52 +0800

    Added some functions
    Added neighbors, neighbor, diagonals, distance, range, ring, and
    hexRound. Line is currently broken

--- HexGrid.elm
+++ HexGrid.elm
@@ -1,6 +1,8 @@
 module HexGrid where
 
-import Public.Preface.Preface (replace, splitAt)
+import Set
+
+import Public.Preface.Preface (replace, splitAt, (#), repeat)
 
 data HexGrid a = Rectangular [[Hex a]] | Hexagonal [[Hex a]]
 type Hex a     = {value : a, coord : HexCoord}
@@ -30,7 +32,7 @@ showHexGrid : HexGrid a -> Element
 showHexGrid grid =
     case grid of
         Rectangular hs -> flow down . map asText . map (\row -> map toTuple row) <| hs
-        Hexagonal   hs -> let rows = map (flow right . map asText) . map (\row -> map toTuple row) <| hs
+        Hexagonal   hs -> let rows = map (flow right . map (\((a, b, c) as x) -> if (show c == "True") then color blue (asText x) else asText x)) . map (\row -> map toTuple row) <| hs
                               l    = length hs
                               w    = widthOf <| head (drop (l `div` 2) rows)
                               rows' = map (\row -> container w (heightOf row) middle row) rows
@@ -56,6 +58,66 @@ inGrid {x, y} grid =
                                  | x > offset - radius - (min 0 y) -> False
                                  | otherwise -> True
 
+neighbors : HexCoord -> [HexCoord]
+neighbors {x, y} = [ HexCoord (x + 1) y, HexCoord (x + 1) (y - 1), HexCoord x (y - 1)
+                   , HexCoord (x - 1) y, HexCoord (x - 1) (y + 1), HexCoord x (y + 1) ]
+
+diagonals : HexCoord -> [HexCoord]
+diagonals {x, y} = [ HexCoord (x + 2) (y - 1), HexCoord (x + 1) (y - 2), HexCoord (x - 1) (y - 1)
+                   , HexCoord (x - 2) (y + 1), HexCoord (x - 1) (y + 2), HexCoord (x + 1) (y + 1) ]   
+
+distance : HexCoord -> HexCoord -> Int
+distance coord1 coord2 =
+    let (x1, y1) = (coord1.x, coord1.y)
+        (x2, y2) = (coord2.x, coord2.y)
+        (z1, z2) = (-(x1 - y1), -(x2 - y2))
+    in  maximum [abs (x1 - x2), abs (y1 - y2), abs (z1 - z2)]
+
+line : HexCoord -> HexCoord -> [HexCoord]
+line coord1 coord2 =
+    let (x1, y1) = ((toFloat coord1.x) + 0.000001, (toFloat coord1.y) + 0.000001)
+        (x2, y2) = (toFloat coord2.x, toFloat coord2.y)
+        (z1, z2) = (-(x1 - y1), -(x2 - y2))
+        dx       = x1 - x2
+        dy       = y1 - y2
+        dz       = z1 - z2
+        n        = round . maximum <| map abs [dx - dy, dy - dz, dz - dx]
+        f x      = (toFloat x) / (toFloat n)
+    in  map (\(a, b) -> HexCoord a b) . Set.toList . Set.fromList <| map (\i -> hexRound (x1 * (f i) + x2 * (1 - (f i)), y1 * (f i) + y2 * (1 - (f i)))) [0..n]
+
+range : HexCoord -> Int -> [HexCoord]
+range {x, y} n = 
+    concatMap (\dx -> 
+        map (\dy -> 
+            HexCoord (x + dx) (y + dy)) 
+        [max -n (-dx - n)..min n (-dx + n)]) 
+    [-n..n]
+
+ring : HexCoord -> Int -> [HexCoord]
+ring {x, y} r =
+    let h = HexCoord (x - r) (y + r) -- move southwest r tiles
+    in  scanl (\i h' ->
+            neighbor h' i)
+            h
+            (concatMap (\j -> repeat j r) [0..5])
+
+neighbor : HexCoord -> Int -> HexCoord
+neighbor {x, y} direction =
+    let (dx, dy) = maybe (0, 0) id <| [ (1, 0), (1, -1), (0, -1)
+                                      , (-1, 0), (-1, 1), (0, 1) ] # direction
+    in  HexCoord (x + dx) (y + dy)
+
+hexRound : (Float, Float) -> (Int, Int)
+hexRound (x, y) =
+    let z            = -(x - y)
+        (rx, ry, rz) = (round x, round y, round z)
+        errX         = abs (rx - x)
+        errY         = abs (ry - y)
+        errZ         = abs (rz - z)
+    in  if | errX > errY && errX > errZ -> ((-ry - rz), ry)
+           | errY > errZ                -> (rx, (-rx - rz))
+           | otherwise                  -> (rx, ry)   
+
 insert : HexCoord -> a -> HexGrid a -> Maybe (HexGrid a)
 insert ({x, y} as coord) z grid = if not <| inGrid coord grid then Nothing else
     case grid of
@@ -65,6 +127,4 @@ insert ({x, y} as coord) z grid = if not <| inGrid coord grid then Nothing else
                               row'   = replace (x + radius + (min 0 y)) (Hex z (HexCoord x y)) row
                           in  Just . Hexagonal <| replace (y + radius) row' hs 
 
-main = (showHexGrid <| rectangularHexGrid (5,3) False)
-       `above`
-       (showHexGrid <| rectangularHexGrid (5, 5) Nothing)
+main = (\n -> showHexGrid <| foldl (\coord grid -> maybe grid id <| insert coord True grid) (hexagonalHexGrid 6 False) <| ring (HexCoord 0 0) n) <~ (count <| every second)
