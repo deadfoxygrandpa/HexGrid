1bb7cfe2a41f95c3bd92e13a1ceb2c1a203b2bff
Alex Neslusan <deadfoxygrandpa@gmail.com>
2014-03-10 18:25:27 +0800

    Update demo for auto resizing

--- Demo.elm
+++ Demo.elm
@@ -24,12 +24,14 @@ styleGuide = Dict.fromList [ (0, SOutlined defaultLine)
 
 scene (x, y) (w, h) selector f txtin s =
     let n' = maybe 3 id <| String.toInt s
-        grid = hexagonalHexGrid 10 0
+        gridSize = 10
+        hexSize = min (100000) ((1 * (toFloat h)) / (3 * gridSize + 2))
+        grid = hexagonalHexGrid gridSize 0
         grid' = foldr (\coord g -> insertIfPossible coord 2 g) grid <| f n' hovered
         grid'' = insertIfPossible hovered 1 grid'
-        griddle = showHexGrid 25 styleGuide <| grid''
+        griddle = color green <| showHexGrid hexSize styleGuide <| grid''
         pos = (x - (w `div` 2), y - (h `div` 2))
-        hovered = pixelToHexCoord 25 pos
-    in  layers [container w h middle griddle, flow down [container w 50 topLeft <| selector `above` txtin, asText hovered]]
+        hovered = pixelToHexCoord hexSize pos
+    in  layers [container w h middle griddle, flow down [container w 50 topLeft <| selector `above` txtin, asText hovered, asText <| sizeOf griddle]]
 
 main = scene <~ Mouse.position ~ Window.dimensions ~ droppa ~ func ~ txt ~ num
