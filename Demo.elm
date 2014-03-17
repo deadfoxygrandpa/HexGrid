4dd8236aecfe0cebab1634637cb0e09aab647602
Alex Neslusan <deadfoxygrandpa@gmail.com>
2014-03-17 18:51:12 +0800

    Update Demo for rectangular hex grid

--- Demo.elm
+++ Demo.elm
@@ -35,7 +35,7 @@ styleGuide rotation sides col n =
 (bggf, bggs) = Input.field "0"
 (bgbf, bgbs) = Input.field "0"
 
-bgcolor = rgb <~ (maybe 0 id . String.toInt <~ bgrs) 
+bgcolor = rgb <~ (maybe 0 id . String.toInt <~ bgrs)
                ~ (maybe 0 id . String.toInt <~ bggs)
                ~ (maybe 0 id . String.toInt <~ bgbs)
 
@@ -43,9 +43,9 @@ bgcolor = rgb <~ (maybe 0 id . String.toInt <~ bgrs)
 (fggf, fggs) = Input.field "156"
 (fgbf, fgbs) = Input.field "0"
 
-fgcolor = rgb <~ (maybe 255 id . String.toInt <~ fgrs) 
+fgcolor = rgb <~ (maybe 255 id . String.toInt <~ fgrs)
                ~ (maybe 156 id . String.toInt <~ fggs)
-               ~ (maybe 0   id . String.toInt <~ fgbs)               
+               ~ (maybe 0   id . String.toInt <~ fgbs)
 
 (rotf, rots) = Input.field "0"
 (sidesf, sidess) = Input.field "6"
@@ -53,12 +53,12 @@ fgcolor = rgb <~ (maybe 255 id . String.toInt <~ fgrs)
 rotation = maybe 0 id . String.toFloat <~ rots
 sides = maybe 6 id . String.toInt <~ sidess
 
-gridsignal = (flip hexagonalHexGrid) 0 <~ gridSizesignal
+gridsignal = (flip rectangularHexGrid) 0 <~ gridSizesignal
 gridSizesignal = maybe 5 id <~ (String.toInt <~ num2)
 
 scene (x, y) (w, h) selector f txtin s txt2in gridSize grid bgrf bggf bgbf bgcolor rotf sidesf fgrf fggf fgbf fgcolor styleGuide =
     let n' = maybe 2 id <| String.toInt s
-        hexSize = min (100000) ((1 * (toFloat h)) / (3 * (toFloat gridSize) + 2))
+        hexSize = (2 * (toFloat h))/(3 * (toFloat gridSize) + 1) --min (100000) ((1 * (toFloat h)) / (3 * (toFloat gridSize) + 2))
         grid' = foldr (\coord g -> insertIfPossible coord 2 g) grid <| f n' hovered
         grid'' = insertIfPossible hovered 1 grid'
         griddle = showHexGrid hexSize styleGuide <| grid''
@@ -71,19 +71,19 @@ scene (x, y) (w, h) selector f txtin s txt2in gridSize grid bgrf bggf bgbf bgcol
                            , flow right <| map (size 100 20) [plainText' "effect size: ", color fgcolor txtin]
                            , flow right <| map (size 100 20) [plainText' "grid size: ", color fgcolor txt2in]
                            , plainText' "background color (rgb):"
-                           , flow right <| intersperse (spacer 5 5) <| 
+                           , flow right <| intersperse (spacer 5 5) <|
                                               map (size 40 20) [ color fgcolor bgrf
                                                                , color fgcolor bggf
                                                                , color fgcolor bgbf
                                                                ]
                            , plainText' "foreground color (rgb):"
-                           , flow right <| intersperse (spacer 5 5) <| 
+                           , flow right <| intersperse (spacer 5 5) <|
                                               map (size 40 20) [ color fgcolor fgrf
                                                                , color fgcolor fggf
                                                                , color fgcolor fgbf
-                                                               ]                                                               
-                           , flow right <| map (size 100 20) [plainText' "rotation:", color fgcolor rotf]     
-                           , flow right <| map (size 100 20) [plainText' "n-gon:", color fgcolor sidesf]       
+                                                               ]
+                           , flow right <| map (size 100 20) [plainText' "rotation:", color fgcolor rotf]
+                           , flow right <| map (size 100 20) [plainText' "n-gon:", color fgcolor sidesf]
                            , plainText' "hex coord your mouse is at:"
                            , plainText' <| show  hovered
                            ]
@@ -93,5 +93,5 @@ scene (x, y) (w, h) selector f txtin s txt2in gridSize grid bgrf bggf bgbf bgcol
                ]
 
 main = scene <~ Mouse.position ~ Window.dimensions ~ droppa ~ func ~ txt ~ num ~ txt2 ~ gridSizesignal ~ gridsignal
-              ~ bgrf ~ bggf ~ bgbf ~ bgcolor ~ rotf ~ sidesf ~ fgrf ~ fggf ~ fgbf ~ fgcolor 
+              ~ bgrf ~ bggf ~ bgbf ~ bgcolor ~ rotf ~ sidesf ~ fgrf ~ fggf ~ fgbf ~ fgcolor
               ~ (styleGuide <~ rotation ~ sides ~ fgcolor)
