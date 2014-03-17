b9e5038176d4c1e6df1cc32edb9c33ee6a590926
Alex Neslusan <deadfoxygrandpa@gmail.com>
2014-03-17 13:33:26 +0800

    Remove some dead code

--- Helpers.elm
+++ Helpers.elm
@@ -1,14 +1,5 @@
 module Helpers where
 
-replace : Int -> a -> [a] -> [a]
-replace n x xs = if (n < 0 || n > (length xs)) then xs else
-    let (ys, zs) = splitAt n xs
-        zs'      = drop 1 zs
-    in  ys ++ (x :: zs')
-
-splitAt : Int -> [a] -> ([a], [a])
-splitAt n xs =  (take n xs, drop n xs)
-
 (#) : [a] -> Int -> Maybe a
 xs # n = case xs of
            []     -> Nothing
