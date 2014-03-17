a11f41fcf7e1cbf877c940d47d6a5a9d8563d4ea
Alex Neslusan <deadfoxygrandpa@gmail.com>
2014-03-18 02:01:32 +0800

    Clean up performance tests

--- Performance.elm
+++ Performance.elm
@@ -28,49 +28,49 @@ styler = styleGuide 25 6 red
 discard _ = 0
 
 port inGridRect : Int -> Int
-port inGridRect = \n -> discard (HexGrid.inGrid (HexGrid.hexCoord 10 10) grid1, n)
+port inGridRect = \n -> discard . HexGrid.inGrid (HexGrid.hexCoord 10 10) <| grid1
 
 port inGridHex : Int -> Int
-port inGridHex = \n -> discard (HexGrid.inGrid (HexGrid.hexCoord 10 10) grid2, n)
+port inGridHex = \n -> discard . HexGrid.inGrid (HexGrid.hexCoord 10 10) <| grid2
 
 port rectangularHexGrid : Int -> Int
-port rectangularHexGrid = \n -> discard (HexGrid.rectangularHexGrid 44 0, n)
+port rectangularHexGrid = \n -> discard . HexGrid.rectangularHexGrid 44 <| 0
 
 port hexagonalHexGrid : Int -> Int
-port hexagonalHexGrid = \n -> discard (HexGrid.hexagonalHexGrid 25 0, n)
+port hexagonalHexGrid = \n -> discard . HexGrid.hexagonalHexGrid 25 <| 0
 
 port showHexGridRect : Int -> Int
-port showHexGridRect = \n -> discard (HexGrid.showHexGrid 25 styler grid1, n)
+port showHexGridRect = \n -> discard . HexGrid.showHexGrid 25 styler <| grid1
 
 port showHexGridHex : Int -> Int
-port showHexGridHex = \n -> discard (HexGrid.showHexGrid 25 styler grid2, n)
+port showHexGridHex = \n -> discard . HexGrid.showHexGrid 25 styler <| grid2
 
 port pixelToHexCoord : Int -> Int
-port pixelToHexCoord = \n -> discard (HexGrid.pixelToHexCoord 25 (50, 50), n)
+port pixelToHexCoord = \n -> discard . HexGrid.pixelToHexCoord 25 <| (50, 50)
 
 port neighbors : Int -> Int
-port neighbors = \n -> discard (HexGrid.neighbors (HexGrid.hexCoord 0 0), n)
+port neighbors = \n -> discard . HexGrid.neighbors <| (HexGrid.hexCoord 0 0)
 
 port diagonals : Int -> Int
-port diagonals = \n -> discard (HexGrid.diagonals (HexGrid.hexCoord 0 0), n)
+port diagonals = \n -> discard . HexGrid.diagonals <| (HexGrid.hexCoord 0 0)
 
 port distance : Int -> Int
-port distance = \n -> discard (HexGrid.distance (HexGrid.hexCoord 10 19) (HexGrid.hexCoord 0 0), n)
+port distance = \n -> discard . HexGrid.distance (HexGrid.hexCoord 10 19) <| (HexGrid.hexCoord 0 0)
 
 port range1 : Int -> Int
-port range1 = \n -> discard (HexGrid.range (HexGrid.hexCoord 0 0) 1, n)
+port range1 = \n -> discard . HexGrid.range (HexGrid.hexCoord 0 0) <| 1
 
 port range10 : Int -> Int
-port range10 = \n -> discard (HexGrid.range (HexGrid.hexCoord 0 0) 10, n)
+port range10 = \n -> discard . HexGrid.range (HexGrid.hexCoord 0 0) <| 10
 
 port ring1 : Int -> Int
-port ring1 = \n -> discard (HexGrid.ring (HexGrid.hexCoord 0 0) 1, n)
+port ring1 = \n -> discard . HexGrid.ring (HexGrid.hexCoord 0 0) <| 1
 
 port ring10 : Int -> Int
-port ring10 = \n -> discard (HexGrid.ring (HexGrid.hexCoord 0 0) 10, n)
+port ring10 = \n -> discard . HexGrid.ring (HexGrid.hexCoord 0 0) <| 10
 
 port insert : Int -> Int
-port insert = \n -> discard (HexGrid.insert (HexGrid.hexCoord 0 0) 1 grid1, n)
+port insert = \n -> discard . HexGrid.insert (HexGrid.hexCoord 0 0) 1 <| grid1
 
 port insertIfPossible : Int -> Int
-port insertIfPossible = \n -> discard (HexGrid.insertIfPossible (HexGrid.hexCoord 0 0) 1 grid1, n)
+port insertIfPossible = \n -> discard . HexGrid.insertIfPossible (HexGrid.hexCoord 0 0) 1 <| grid1
