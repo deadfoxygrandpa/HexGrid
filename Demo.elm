ecba44a48a6f3c15b5dba0466bf3ee3b19a35b94
Alex Neslusan <deadfoxygrandpa@gmail.com>
2014-03-21 13:59:01 +0800

    Significantly clean up demo

--- Demo.elm
+++ Demo.elm
@@ -20,82 +20,137 @@ styleGuide rotation sides col n =
                     _ -> scale 0.9 . rotate (degrees <| rotation + 30) . colorize . ngon sides -- colorize . square
     in shape
 
-list x = [x]
+-- Input Controls:
 
-(droppa, func) = Input.dropDown [ ("Diagonals", \_ -> diagonals)
-                                , ("Ring", flip ring)
-                                , ("Range", flip range)
-                                , ("Neighbors", \_ -> neighbors)
-                                , ("Line", \_ -> line (hexCoord 0 0))
-                                , ("Rotate Left", \_ coord -> [rotation Left coord, hexCoord 0 0])
-                                , ("Rotate Right", \_ coord -> [rotation Right coord, hexCoord 0 0])
-                                , ("None", \_ _ -> [])
-                                ]
-(txt, num) = Input.field "2"
+(gridTypeBox, gridType) = Input.dropDown
+    [ ("Rectangular", True)
+    , ("Hexagonal"  , False)
+    ]
+(effectBox, effect) = Input.dropDown
+    [ ("Diagonals"   , \_ -> diagonals)
+    , ("Ring"        , flip ring)
+    , ("Range"       , flip range)
+    , ("Neighbors"   , \_ -> neighbors)
+    , ("Line"        , \_ -> line (hexCoord 0 0))
+    , ("Rotate Left" , \_ coord -> [rotation Left coord, hexCoord 0 0])
+    , ("Rotate Right", \_ coord -> [rotation Right coord, hexCoord 0 0])
+    , ("None"        , \_ _ -> [])
+    ]
+(effectSizeBox, effectSizeString) = Input.field "2"
+(gridSizeBox  , gridSizeString)   = Input.field "5"
+(bgRedBox     , bgRedString)      = Input.field "0"
+(bgGreenBox   , bgGreenString)    = Input.field "0"
+(bgBlueBox    , bgBlueString)     = Input.field "0"
+(fgRedBox     , fgRedString)      = Input.field "255"
+(fgGreenBox   , fgGreenString)    = Input.field "156"
+(fgBlueBox    , fgBlueString)     = Input.field "0"
+(rotationBox  , rotationString)   = Input.field "0"
+(sidesBox     , sidesString)      = Input.field "6"
 
-(txt2, num2) = Input.field "5"
+-- Input Signals:
 
-(bgrf, bgrs) = Input.field "0"
-(bggf, bggs) = Input.field "0"
-(bgbf, bgbs) = Input.field "0"
+effectSize = maybe 2 id . String.toInt <~ effectSizeString
+gridSize = (,) <~ (maybe 5 id . String.toInt <~ gridSizeString)
+                ~ (maybe 5 id . String.toInt <~ gridSizeString)
+bgColor = rgb <~ (maybe 0   id . String.toInt   <~ bgRedString)
+               ~ (maybe 0   id . String.toInt   <~ bgGreenString)
+               ~ (maybe 0   id . String.toInt   <~ bgBlueString)
+fgColor = rgb <~ (maybe 255 id . String.toInt   <~ fgRedString)
+               ~ (maybe 156 id . String.toInt   <~ fgGreenString)
+               ~ (maybe 0   id . String.toInt   <~ fgBlueString)
+rotationOffest =  maybe 0   id . String.toFloat <~ rotationString
+sides          =  maybe 6   id . String.toInt   <~ sidesString
 
-bgcolor = rgb <~ (maybe 0 id . String.toInt <~ bgrs)
-               ~ (maybe 0 id . String.toInt <~ bggs)
-               ~ (maybe 0 id . String.toInt <~ bgbs)
+grid = (\s t -> if t then rectangularHexGrid s 0 else hexagonalHexGrid (fst s) 0) <~ gridSize ~ gridType
 
-(fgrf, fgrs) = Input.field "255"
-(fggf, fggs) = Input.field "156"
-(fgbf, fgbs) = Input.field "0"
+hexSize = (\h gridSize t -> if t
+    then (2 * (toFloat h))/(3 * (toFloat . fst <| gridSize) + 1)
+    else min (100000) ((1 * (toFloat h)) / (3 * (toFloat . fst <| gridSize) + 2))
+    ) <~ Window.height ~ gridSize ~ gridType
 
-fgcolor = rgb <~ (maybe 255 id . String.toInt <~ fgrs)
-               ~ (maybe 156 id . String.toInt <~ fggs)
-               ~ (maybe 0   id . String.toInt <~ fgbs)
+type Environment = { mouse : (Int, Int)
+                   , window : (Int, Int)
+                   , gridTypeBox : Element
+                   , effectBox : Element
+                   , effect : Int -> (Int, Int) -> [HexCoord]
+                   , effectSizeBox : Element
+                   , effectSize : Int
+                   , gridSizeBox : Element
+                   , hexSize : Float
+                   , grid : HexGrid Int
+                   , bgRedBox : Element
+                   , bgGreenBox : Element
+                   , bgBlueBox : Element
+                   , bgColor : Color
+                   , rotationBox : Element
+                   , sidesBox : Element
+                   , fgRedBox : Element
+                   , fgGreenBox : Element
+                   , fgBlueBox : Element
+                   , fgColor : Color
+                   , styleGuide : Int -> Float -> Form
+                   }
 
-(rotf, rots) = Input.field "0"
-(sidesf, sidess) = Input.field "6"
+environment : Signal Environment
+environment = Environment <~ Mouse.position
+                           ~ Window.dimensions
+                           ~ gridTypeBox
+                           ~ effectBox
+                           ~ effect
+                           ~ effectSizeBox
+                           ~ effectSize
+                           ~ gridSizeBox
+                           ~ hexSize
+                           ~ grid
+                           ~ bgRedBox
+                           ~ bgGreenBox
+                           ~ bgBlueBox
+                           ~ bgColor
+                           ~ rotationBox
+                           ~ sidesBox
+                           ~ fgRedBox
+                           ~ fgGreenBox
+                           ~ fgBlueBox
+                           ~ fgColor
+                           ~ (styleGuide <~ rotationOffest ~ sides ~ fgColor)
 
-rotationValue = maybe 0 id . String.toFloat <~ rots
-sides = maybe 6 id . String.toInt <~ sidess
-
-gridsignal = (flip rectangularHexGrid) 0 <~ gridSizesignal
-gridSizesignal = (,) <~ (maybe 5 id . String.toInt <~ num2) ~ (maybe 5 id . String.toInt <~ num2)
-
-scene (x, y) (w, h) selector f txtin s txt2in gridSize grid bgrf bggf bgbf bgcolor rotf sidesf fgrf fggf fgbf fgcolor styleGuide =
-    let n' = maybe 2 id <| String.toInt s
-        hexSize = (2 * (toFloat h))/(3 * (toFloat . fst <| gridSize) + 1) --min (100000) ((1 * (toFloat h)) / (3 * (toFloat gridSize) + 2))
-        grid' = foldr (\coord g -> insertIfPossible coord 2 g) grid <| f n' hovered
+scene environment =
+    let (x, y) = environment.mouse
+        (w, h) = environment.window
+        bgColor = environment.bgColor
+        fgColor = environment.fgColor
+        grid' = foldr (\coord g -> insertIfPossible coord 2 g) environment.grid <| environment.effect environment.effectSize hovered
         grid'' = insertIfPossible hovered 1 grid'
-        griddle = showHexGrid hexSize styleGuide <| grid''
+        griddle = showHexGrid environment.hexSize environment.styleGuide <| grid''
         pos = (x - (w `div` 2), y - (h `div` 2))
-        hovered = pixelToHexCoord hexSize pos
-        plainText' = text . Text.color fgcolor . toText
-        panel = flow down [ color fgcolor <| container 200 30 midLeft <| text . Text.color bgcolor . bold . toText <| "control panel:"
+        hovered = pixelToHexCoord environment.hexSize pos
+        plainText' = text . Text.color fgColor . toText
+        panel = flow down [ color fgColor <| container 200 30 midLeft <| text . Text.color bgColor . bold . toText <| "control panel:"
                            , spacer 2 2
-                           , flow right <| map (size 100 20) [plainText' "effect:", color fgcolor selector]
-                           , flow right <| map (size 100 20) [plainText' "effect size: ", color fgcolor txtin]
-                           , flow right <| map (size 100 20) [plainText' "grid size: ", color fgcolor txt2in]
+                           , flow right <| map (size 100 20) [plainText' "grid type:", color fgColor environment.gridTypeBox]
+                           , flow right <| map (size 100 20) [plainText' "effect:", color fgColor environment.effectBox]
+                           , flow right <| map (size 100 20) [plainText' "effect size: ", color fgColor environment.effectSizeBox]
+                           , flow right <| map (size 100 20) [plainText' "grid size: ", color fgColor environment.gridSizeBox]
                            , plainText' "background color (rgb):"
                            , flow right <| intersperse (spacer 5 5) <|
-                                              map (size 40 20) [ color fgcolor bgrf
-                                                               , color fgcolor bggf
-                                                               , color fgcolor bgbf
+                                              map (size 40 20) [ color fgColor environment.bgRedBox
+                                                               , color fgColor environment.bgGreenBox
+                                                               , color fgColor environment.bgBlueBox
                                                                ]
                            , plainText' "foreground color (rgb):"
                            , flow right <| intersperse (spacer 5 5) <|
-                                              map (size 40 20) [ color fgcolor fgrf
-                                                               , color fgcolor fggf
-                                                               , color fgcolor fgbf
+                                              map (size 40 20) [ color fgColor environment.fgRedBox
+                                                               , color fgColor environment.fgGreenBox
+                                                               , color fgColor environment.fgBlueBox
                                                                ]
-                           , flow right <| map (size 100 20) [plainText' "rotation:", color fgcolor rotf]
-                           , flow right <| map (size 100 20) [plainText' "n-gon:", color fgcolor sidesf]
+                           , flow right <| map (size 100 20) [plainText' "rotation:", color fgColor environment.rotationBox]
+                           , flow right <| map (size 100 20) [plainText' "n-gon:", color fgColor environment.sidesBox]
                            , plainText' "hex coord your mouse is at:"
-                           , plainText' <| show  hovered
+                           , plainText' <| show hovered
                            ]
-    in  layers [ color bgcolor <| container w h middle griddle
-               , color bgcolor <| spacer (widthOf panel + 3) (heightOf panel + 3)
+    in  layers [ color bgColor <| container w h middle griddle
+               , color bgColor <| spacer (widthOf panel + 3) (heightOf panel + 3)
                , container (widthOf panel + 5) (heightOf panel + 5) middle panel
                ]
 
-main = scene <~ Mouse.position ~ Window.dimensions ~ droppa ~ func ~ txt ~ num ~ txt2 ~ gridSizesignal ~ gridsignal
-              ~ bgrf ~ bggf ~ bgbf ~ bgcolor ~ rotf ~ sidesf ~ fgrf ~ fggf ~ fgbf ~ fgcolor
-              ~ (styleGuide <~ rotationValue ~ sides ~ fgcolor)
+main = scene <~ environment
